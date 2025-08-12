653ff757408fab9c74c812413a275c95fa368f39
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2025-08-12 14:07:54 -0600

    Cambios

--- src/main/java/io/github/alfregood/to_dolist/servicio/UsuarioServ.java
+++ src/main/java/io/github/alfregood/to_dolist/servicio/UsuarioServ.java
@@ -2,6 +2,7 @@ package io.github.alfregood.to_dolist.servicio;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,30 +16,38 @@ public class UsuarioServ {
     @Autowired
     private UsuarioRepo repositorio;
 
-    public Usuario guardar(Usuario usuario){
+    public Usuario guardar(Usuario usuario) {
         return repositorio.save(usuario);
     }
 
-    public Usuario validarCredenciales(String correo, String contrasena){
-        Optional<Usuario>usuario=repositorio.findByCorreoAndContrasena(correo, contrasena);
+    public Usuario validarCredenciales(String correo, String contrasena) {
+        Optional<Usuario> usuario = repositorio.findByCorreoAndContrasena(correo, contrasena);
 
-        if(!usuario.isPresent()){
+        if (!usuario.isPresent()) {
             return null;
         }
 
         return usuario.get();
     }
 
-    public Usuario obtenerPorId(long id){
-        Optional<Usuario>usuario=repositorio.findById(id);
+    public Usuario obtenerPorId(long id) {
+        Optional<Usuario> usuario = repositorio.findById(id);
 
-        if(!usuario.isPresent()){
+        if (!usuario.isPresent()) {
             return null;
         }
         return usuario.get();
     }
 
-    public List<Usuario> listaUsuarios(){
-        return repositorio.findAll();
+    public boolean igualarCorreos(String correoUsuario) {
+        List<String> correos = repositorio.findAll().stream().map(u -> u.getCorreo()).collect(Collectors.toList());
+
+        for (String correo : correos) {
+            if (correo.equalsIgnoreCase(correoUsuario)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
